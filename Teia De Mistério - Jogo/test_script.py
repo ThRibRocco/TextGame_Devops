947253f02bb5d379f3025131cdc5610652b9eeda
Lu.tamura <140550179+lu-tamura@users.noreply.github.com>
2024-06-17 22:52:59 -0300

    Update test_script.py

--- Teia De Mistério - Jogo/test_script.py
+++ Teia De Mistério - Jogo/test_script.py
@@ -1,8 +1,59 @@
 import sys
 import time
 import os
-from TeiaDeMistério_Completo import digitar_texto, exibir_fala, narra, fim
 
+# FUNC PRA DIGITAR TEXTO DE UM JEITO BONITINHO
+def digitar_texto(texto):
+    estilo_cor = "\033[1;32m"
+    reset_cor = "\033[0m"
+
+    for char in texto:
+        sys.stdout.write(estilo_cor + char + reset_cor)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print()
+
+# FUNC PRA EXIBIR A FALA DE UM JEITO BONITINHO
+def exibir_fala(nome_personagem, fala, cor):
+    estilo_fala = f"\033[1;{cor}m"
+    reset_cor = "\033[0m"
+
+    print(f"{estilo_fala}{nome_personagem}:{reset_cor}")
+
+    for linha in fala:
+        for char in linha:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.05)
+        print()
+        time.sleep(0.5)
+
+#FUNCAO PARA EXIBIR AS NARRAÇÕES
+def narra(texto):
+    for linha in texto:
+        print(linha)
+        time.sleep(1)
+    print()
+
+#FUNCAO PARA OS CREDITOS
+def fim():
+    creditos = [
+        'TEIA DE MISTÉRIO',
+        'Por - Saturn Games',
+        'Obrigado por jogar!'
+    ]
+
+    largura_tela = os.get_terminal_size().columns
+
+    for linha in creditos:
+        espacos = (largura_tela - len(linha)) // 2
+        linha_formatada = ' ' * espacos + linha
+
+        print(linha_formatada)
+        time.sleep(2)
+
+
+# TESTES
 def test_digitar_texto():
     print("Testing digitar_texto function...")
     texto = "Testando a função digitar_texto."
